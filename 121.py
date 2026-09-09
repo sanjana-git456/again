e0c612934d436b3d06d2fad8c1a47c7225d9fd70
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-09-09 23:54:20 +0530

    profit

---
+++ 121.py
@@ -0,0 +1,13 @@
+x = list(map(int, input("Enter: ").split()))
+def profit(x):
+    left = 0
+    right = len(x)-1
+    m = x[right] - x[left]
+    while left < right:
+        new = x[right] - x[left]
+        m = min(m,new)
+    if m < 0:
+        return 0
+    else:
+        return m
+print(profit(x))
