4fcf72dc5cdf7668f7153c3f8e84b58186a46ccb
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-09-10 00:09:53 +0530

    profit

--- 121.py
+++ 121.py
@@ -2,10 +2,9 @@ x = list(map(int, input("Enter: ").split()))
 def profit(x):
     minprice = x[0]
     maxprofit = 0
-    for i in range(len(x)):
+    for i in range(1,len(x)):
         diff = x[i]-minprice
         maxprofit = max(maxprofit,diff)
-    if x[i] < minprice:
-        minprice = x[i]
+        minprice = min(minprice,x[i])
     return maxprofit
 print(profit(x))
