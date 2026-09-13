4d56671b08a532d1b50d9319850483f956a14fc8
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-09-13 18:17:21 +0530

    three sum

--- 15.py
+++ 15.py
@@ -1,15 +1,16 @@
 nums = list(map(int, input("Enter: ").split()))
-target = int(input("Enter target: "))
-def ts(x,t):
-    left = 0
-    right = len(x)
-    for i in range(len(x)):
-        fix = t - x[i]
-        x = sorted(x)
-        if x[left] + x[right] == fix:
-            return left,right,i
-        elif x[left] + x[right] > fix:
-            right -= 1
-        else:
-            left += 1
-    
+def ts(x):
+    x = sorted(x)
+    for i in range(len(x)-1):
+        left = i+1
+        right = len(x)-1
+        fix = x[i]
+        find = -fix
+        while left < right:
+            if x[left]+x[right] == find:
+                return left, right, i
+            elif x[left]+x[right] > find:
+                right -= 1
+            else:
+                left += 1
+print(ts(nums))
