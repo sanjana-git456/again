f80111411985fe33632ee1426659ab9c1021c27d
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-09-19 19:34:16 +0530

    rotate array

--- 189.py
+++ 189.py
@@ -2,10 +2,10 @@ x = list(map(int, input("Enter: ").split()))
 k = int(input("Enter: "))
 def rotate(x,k):
     if k > len(x):
-        return False
+        k = k % len(x)
     l1 = []
     l2 = []
-    l1 += x[k:]
-    l2 += x[:k]
+    l1 += x[-k:]
+    l2 += x[:-k]
     return l1+l2
 print(rotate(x,k))
