232a9d5455e536775f6c890f693b8eb49186cd15
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-09-19 20:45:37 +0530

    island problem

---
+++ 200.py
@@ -0,0 +1,22 @@
+grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+
+def island(x,r,c):
+    if r < 0 or r >= len(x) or c < 0 or c >= len(x[0]) or x[r][c] != '1':
+        return
+    x[r][c] = 0
+    island(x,r-1,c) #up
+    island(x,r,c+1) #right
+    island(x,r+1,c) #down
+    island(x,r,c-1) #left
+def num(x,r,c):
+    count = 0
+    for r in range(len(x)):
+        for c in range(len(x[0])):
+            count += 1
+            island(x,r,c)
+    return count
