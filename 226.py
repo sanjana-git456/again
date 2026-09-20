d76e6a0557984c78a4747d74960fa061b4aede48
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-09-20 22:42:26 +0530

    invert tree

---
+++ 226.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right = TreeNode(7)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(9)
+
+def invert(root):
+    root.left, root.right = root.right, root.left
+    return root
+print(invert(root))
